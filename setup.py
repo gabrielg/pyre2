5d09de648e8a049c410cafe113a63f4f7f957210
Gabriel Gironda <gabriel@thegroundwork.com>
2016-05-02 17:34:33 -0500

    Vendor re2 and add support for building pyre2 statically linked against it
    `setup.py install --vendored-re2` will build and link against a vendored re2
    version.

--- setup.py
+++ setup.py
@@ -1,10 +1,69 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+from distutils.command.install import install as _install
+
+
+_VENDORED_RE2 = 're2-2016-05-01'
+
+
+class InstallCommand(_install):
+    boolean_options = _install.boolean_options + ['vendored-re2']
+    user_options = _install.user_options + [
+        ('vendored-re2', None, 'statically link against vendored re2'),
+    ]
+
+    def initialize_options(self):
+        _install.initialize_options(self)
+        self.vendored_re2 = False
+
+    def run(self):
+        if self.vendored_re2:
+            import os
+            import tempfile
+
+
+            tmp_install_prefix = tempfile.mkdtemp()
+
+            self.spawn([
+                'make',
+                '-C',
+                _VENDORED_RE2,
+                'install',
+                'prefix=%s' % tmp_install_prefix,
+            ])
+
+            include_dirs = ['%s/include' % tmp_install_prefix]
+            extra_objects = ['%s/lib/libre2.a' % tmp_install_prefix]
+
+            ext_modules = [
+                Extension('_re2',
+                    sources = ['_re2.cc'],
+                    include_dirs = include_dirs,
+                    extra_objects = extra_objects,
+                    extra_compile_args=['-std=c++11'],
+                ),
+            ]
+        else:
+            ext_modules = [
+                Extension('_re2',
+                    sources = ['_re2.cc'],
+                    libraries = ['re2'],
+                    extra_compile_args=['-std=c++11'],
+                ),
+            ]
+
+        self.distribution.ext_modules = ext_modules
+
+        return _install.run(self)
+
 
 setup(
+    cmdclass={
+        'install': InstallCommand,
+    },
     name="fb-re2",
-    version="1.0.4",
+    version="1.1.0",
     url="https://github.com/facebook/pyre2",
     description="Python wrapper for Google's RE2",
     classifiers=[
@@ -17,9 +76,4 @@ setup(
     maintainer="Siddharth Agarwal",
     maintainer_email="sid0@fb.com",
     py_modules = ["re2"],
-    ext_modules = [Extension("_re2",
-      sources = ["_re2.cc"],
-      libraries = ["re2"],
-      extra_compile_args=['-std=c++11'],
-      )],
     )
